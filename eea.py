8d71560bca43d70545a3e8fa2a46f389a5349bb0
hiram-pl <hiramlannes@gmail.com>
2026-07-29 13:46:11 +0200

    New function for comparing different approaches

--- eea.py
+++ eea.py
@@ -10,3 +10,23 @@ def disco(sql, p=1, n=1000):
     if "errors" in j:
         raise RuntimeError(j["errors"])
     return pd.DataFrame(j["results"])
+
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+# Function for comparing different approaches
+def score_rf_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
+
+
+from xgboost import XGBoostRegressor
+from sklearn.metrics import mean_absolute_error
+# Function for comparing different approaches
+def score_xgb_dataset(X_train, X_valid, y_train, y_valid):
+    model = XGBoostRegressor(n_estimators=100, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
